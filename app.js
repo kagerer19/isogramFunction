2a1e8f864c4e171c50af409c6462c261fd1da031
Alexander kagerer <kagerer19@gmail.com>
2023-09-15 07:46:01 +0200

    Add Iso-gram checker

---
+++ app.js
@@ -0,0 +1,22 @@
+function countStr(str, letter) {
+    let count = 0;
+
+    //Loop through String
+    for(let i = 0; i< str.length; i++){
+
+        //Check chars at pos
+        if (str.charAt(i) === letter){
+            count += 1;
+        }
+    }
+    return count;
+}
+
+//JS promt
+const string = prompt("Enter a string: ");
+const letterToCheck = prompt("Enter a letter to check: ");
+
+const result = countStr(string, letterToCheck);
+
+//display result
+alert(result);
